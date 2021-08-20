53c8e929e7da96a1e135214fcb2241ed79eb75bf
Sergei <sergey2716@gmail.com>
2021-08-21 00:36:57 +0400

    Cтруктура проекта, ещё раз про ui - bll, пробрасываем пропсы

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,17 +2,10 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-
-    let posts = [
-        {id: 1, message: 'Hi, how are you?', likeCount:'12'},
-        {id: 2, message: 'It\'s my first post', likeCount:'7'},
-        {id: 3, message: 'BlaBla', likeCount:'3'},
-        {id: 4, message: 'FooBar', likeCount:'6'}
-    ]
+const MyPosts  = (props) => {
 
     let postsElems =
-        posts.map( p => <Post message={p.message} likeCount={p.likeCount} /> )
+       props.p.map( p => <Post message={p.message} likeCount={p.likeCount} /> )
 
     return <div className={s.postsBlock}>
         <div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,9 +7,17 @@ const Profile = () => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts p={posts} />
         </div>
     )
 }
+
+let posts = [
+    {id: 1, message: 'Hi, how are you?', likeCount:'12'},
+    {id: 2, message: 'It\'s my first post', likeCount:'7'},
+    {id: 3, message: 'BlaBla', likeCount:'3'},
+    {id: 4, message: 'FooBar', likeCount:'6'}
+]
+
 
 export default Profile
