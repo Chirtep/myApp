54ce2ccde7184fa38a88e1e6f9c4bd8bfb35655c
Sergei <sergey2716@gmail.com>
2021-09-16 23:03:56 +0400

    Вводим пагинацию страницы пользователей (Users)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,16 +5,42 @@ import userPhoto from '../../assets/images/logo-user-icon.png'
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            this.props.setUsers(response.data.items);
-        });
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
     render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i = Math.max(1, this.props.currentPage - 3 ); i <= Math.min(this.props.currentPage + 3, pagesCount); i++) {
+            pages.push(i)
+        }
+
         return (
             <div>
+                <div>
+                    {pages.map(p => {
+                        let chosen = this.props.currentPage === p ? styles.selectedPage : this.props.currentPage;
+                        return <span className={chosen + ' ' + styles.pointer}
+                                     onClick={(e) => {
+                                         this.onPageChanged(p)
+                                     }}>{p}
+                        </span>
+                    })}
+                </div>
                 {
                     this.props.users.map(u => <div key={u.id}>
                 <span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,8 +1,13 @@
 const TOGGLE_FOLLOW = 'TOGGLE_FOLLOW';
-const SET_USERS = 'SET_USERS'
+const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -11,7 +16,7 @@ const usersReducer = (state = initialState, action) => {
         case TOGGLE_FOLLOW:
             return {
                 ...state,
-                users: state.users.map(u =>  {
+                users: state.users.map(u => {
                     if (u.id === action.userID) {
                         return {...u, followed: !u.followed}
                     }
@@ -20,7 +25,15 @@ const usersReducer = (state = initialState, action) => {
             }
 
         case SET_USERS: {
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: action.users}
+        }
+
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
+        }
+
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.totalUsersCount}
         }
 
         default:
@@ -30,5 +43,7 @@ const usersReducer = (state = initialState, action) => {
 
 export const toggleFollowAC = (userID) => ({type: TOGGLE_FOLLOW, userID})
 export const setUsersAC = (users) => ({type: SET_USERS, users})
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, totalUsersCount})
 
 export default usersReducer;
