e9a4336bd7a239a92abaf5e10fb4ebcf05f2c312
Sergei <sergey2716@gmail.com>
2021-09-27 00:11:07 +0400

    Пробуем redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import {Route} from "react-router-dom";
 import Music from "./components/Music/Music";
@@ -10,6 +9,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import LoginPage from "./components/Login/Login";
 
 const App = () => {
 
@@ -38,6 +38,9 @@ const App = () => {
                 <Route path='/settings'
                        render={() =>
                            <Settings/>}/>
+                <Route path='/login'
+                       render={() =>
+                           <LoginPage/>}/>
             </div>
         </div>
     );
