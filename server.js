45828900dbce351e2a6ebea6510916bbb31bd6ff
Sergei <sergey2716@gmail.com>
2022-02-09 15:56:48 +0400

    технические правки

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 
 
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/build/favicon.png'));
 
 
 app.use(express.static(__dirname));
